f385c422519bc5d9d2895b1f0af6d7800367bea9
dhimaspp <dhimasputera@gmail.com>
2021-03-25 01:15:10 +0700

    update model

--- wit_api/settings.py
+++ wit_api/settings.py
@@ -121,12 +121,12 @@ WSGI_APPLICATION = 'wit_api.wsgi.application'
 #     print 'Unexpected error:', sys.exc_info()
 # DATABASES = {}
 # DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 # DATABASES = {
