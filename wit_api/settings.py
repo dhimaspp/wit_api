fc82094ae106c3d572df6da19f56cd0f4231837f
dhimaspp <dhimasputera@gmail.com>
2021-03-25 08:18:25 +0700

    move to postgresql

--- wit_api/settings.py
+++ wit_api/settings.py
@@ -123,10 +123,20 @@ WSGI_APPLICATION = 'wit_api.wsgi.application'
 # DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Pangestu123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 # DATABASES = {
